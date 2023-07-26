4268b0114fd602adb92cbc0ed1d8f0d04cfdde4f
Andrea Botello <andrea.botellor@misena.edu.co>
2023-07-26 17:11:43 -0500

    material

--- src/config/global.js
+++ src/config/global.js
@@ -126,7 +126,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/222319_CF012_DU.pdf',
       },
-
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
